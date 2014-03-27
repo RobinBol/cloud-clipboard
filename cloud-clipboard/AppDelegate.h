77a1446716538a2e9fb7c6f4472aa72d3008d373
Robin Bolscher <robinbolscher@gmail.com>
2014-03-27 13:54:00 +0100

    base commit

---
+++ cloud-clipboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cloud-clipboard
+//
+//  Created by Robin Bolscher on 27-03-14.
+//  Copyright (c) 2014 GreenCoding. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
